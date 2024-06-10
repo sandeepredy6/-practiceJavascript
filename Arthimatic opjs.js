9d44af8320d0ce4c0005fddd88fe4bd0ac1c7aa9
sandeepredy6 <145992263+sandeepredy6@users.noreply.github.com>
2024-06-10 10:13:03 +0530

    Add files via upload

---
+++ Arthimatic opjs.js
@@ -0,0 +1,21 @@
+let a=13;
+
+let b=8;
+
+console.log(a+b);
+
+console.log(a-b);
+
+console.log(a*b);
+
+console.log(a/b);
+
+console.log(a%b);
+
+console.log(a++);
+
+console.log(++a);
+
+console.log(b--);
+
+console.log(--b);
